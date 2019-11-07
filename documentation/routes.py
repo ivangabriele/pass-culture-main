4483c20fc5cae1d4121d3fb99a056c1035a19564
Adrien SAUNIER <contact.adriensaunier@gmail.com>
2019-11-07 16:42:01 +0100

    (PC-2537): Fix conflict on documentation + code review

--- documentation/routes.py
+++ documentation/routes.py
@@ -2,13 +2,13 @@ from flask import send_from_directory
 from flask import current_app as app
 
 
-@app.route('/api/doc')
-def api_documentation_index():
+@app.route('/api/doc/')
+def api_documentation():
     return send_from_directory('static/documentation', 'index.html')
 
 
 @app.route('/api/doc/<path:path>')
-def api_documentation(path):
+def static_files(path):
     if '.json' in path:
         return send_from_directory('documentation', path)
 
