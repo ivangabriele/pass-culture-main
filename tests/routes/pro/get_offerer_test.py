ivangabriele/pass-culture-main

commit 5ea81c3fd6a60b923120b1f81a217418505a34d7

(PC-6840): Move GET /offerer to spectree

src/pcapi/routes/serialization/offerers_serialize.py
@@ -1,0 +1,70 @@
+from datetime import datetime
+from typing import List
+from typing import Optional
+
+from pydantic import BaseModel
+
+from pcapi.serialization.utils import humanize_field
+from pcapi.utils.date import format_into_utc_date
+
+
+class GetOffererVenueResponseModel(BaseModel):
+    address: Optional[str]
+    bic: Optional[str]
+    bookingEmail: Optional[str]
+    city: Optional[str]
+    comment: Optional[str]
+    dateCreated: datetime
+    dateModifiedAtLastProvider: Optional[datetime]
+    departementCode: Optional[str]
+    iban: Optional[str]
+    id: str
+    idAtProviders: Optional[str]
+    isValidated: bool
+    isVirtual: bool
+    lastProviderId: Optional[str]
+    latitude: Optional[float]
+    longitude: Optional[float]
+    managingOffererId: str
+    name: str
+    nOffers: int
+    postalCode: Optional[str]
+    publicName: Optional[str]
+    siret: Optional[str]
+    venueLabelId: Optional[str]
+    venueTypeId: Optional[str]
+
+    _humanize_id = humanize_field("id")
+    _humanize_managing_offerer_id = humanize_field("managingOffererId")
+    _humanize_venue_label_id = humanize_field("venueLabelId")
+    _humanize_venue_type_id = humanize_field("venueTypeId")
+
+    class Config:
+        orm_mode = True
+        json_encoders = {datetime: format_into_utc_date}
+
+
+class GetOffererResponseModel(BaseModel):
+    address: Optional[str]
+    bic: Optional[str]
+    city: str
+    dateCreated: datetime
+    dateModifiedAtLastProvider: Optional[datetime]
+    fieldsUpdated: List[str]
+    iban: Optional[str]
+    id: str
+    idAtProviders: Optional[str]
+    isValidated: bool
+    lastProviderId: Optional[str]
+    managedVenues: List[GetOffererVenueResponseModel]
+    name: str
+    nOffers: int
+    postalCode: str
+    # FIXME (dbaty, 2020-11-09): optional until we populate the database (PC-5693)
+    siren: Optional[str]
+
+    _humanize_id = humanize_field("id")
+
+    class Config:
+        orm_mode = True
+        json_encoders = {datetime: format_into_utc_date}

tests/routes/pro/get_offerer_test.py
@@ -1,11 +1,8 @@
 import pytest
 
+import pcapi.core.offers.factories as offers_factories
+import pcapi.core.users.factories as users_factories
 from pcapi.model_creators.generic_creators import create_bank_information
-from pcapi.model_creators.generic_creators import create_offerer
-from pcapi.model_creators.generic_creators import create_user
-from pcapi.model_creators.generic_creators import create_user_offerer
-from pcapi.model_creators.generic_creators import create_venue
-from pcapi.repository import repository
 from pcapi.utils.human_ids import humanize
 
 from tests.conftest import TestClient
@@ -16,12 +13,11 @@ class Get:
         @pytest.mark.usefixtures("db_session")
         def when_user_offerer_does_not_exist(self, app):
             # Given
-            user = create_user()
-            repository.save(user)
+            pro = users_factories.UserFactory(isBeneficiary=False)
             invalid_id = 12
 
             # When
-            response = TestClient(app.test_client()).with_auth(user.email).get("/offerers/%s" % invalid_id)
+            response = TestClient(app.test_client()).with_auth(pro.email).get("/offerers/%s" % invalid_id)
 
             # then
             assert response.status_code == 404
@@ -31,18 +27,21 @@ class Get:
         @pytest.mark.usefixtures("db_session")
         def when_user_has_rights_on_offerer(self, app):
             # given
-            user = create_user()
-            offerer = create_offerer()
-            venue = create_venue(offerer)
+            pro = users_factories.UserFactory(isBeneficiary=False)
+            venue = offers_factories.VenueFactory()
+            offers_factories.UserOffererFactory(user=pro, offerer=venue.managingOfferer)
+
             create_bank_information(venue=venue)
-            user_offerer = create_user_offerer(user, offerer)
-            repository.save(user_offerer, venue)
+
             # when
-            response = TestClient(app.test_client()).with_auth(user.email).get(f"/offerers/{humanize(offerer.id)}")
+            response = (
+                TestClient(app.test_client())
+                .with_auth(pro.email)
+                .get(f"/offerers/{humanize(venue.managingOfferer.id)}")
+            )
 
             # then
             assert response.status_code == 200
             response_json = response.json
             assert "bic" in response_json["managedVenues"][0]
             assert "iban" in response_json["managedVenues"][0]
-            assert response_json["userHasAccess"] is True

src/pcapi/routes/pro/offerers.py
@@ -20,6 +20,8 @@ from pcapi.models.venue_sql_entity import create_digital_venue
 from pcapi.repository import repository
 from pcapi.repository.offerer_queries import find_by_siren
 from pcapi.routes.serialization import as_dict
+from pcapi.routes.serialization.offerers_serialize import GetOffererResponseModel
+from pcapi.serialization.decorator import spectree_serialize
 from pcapi.use_cases.list_offerers_for_pro_user import OfferersRequestParameters
 from pcapi.utils.human_ids import dehumanize
 from pcapi.utils.includes import OFFERER_INCLUDES
@@ -76,14 +78,14 @@ def list_offerers():
     return response, 200
 
 
-# @debt api-migration
 @private_api.route("/offerers/<offerer_id>", methods=["GET"])
 @login_required
-def get_offerer(offerer_id):
+@spectree_serialize(response_model=GetOffererResponseModel)
+def get_offerer(offerer_id: str) -> GetOffererResponseModel:
     ensure_current_user_has_rights(RightsType.editor, dehumanize(offerer_id))
     offerer = load_or_404(Offerer, offerer_id)
 
-    return jsonify(get_dict_offerer(offerer)), 200
+    return GetOffererResponseModel.from_orm(offerer)
 
 
 # @debt api-migration

commit eb4cb98dbc9f252ba26ed379da0fa74a0acc8f42

[PC-6840] route.offerers: add missing field "demarchesSimplifieesApplicationId"
  regression from 5ea81c3f

src/pcapi/routes/serialization/offerers_serialize.py
@@ -50,6 +50,7 @@ class GetOffererResponseModel(BaseModel):
     city: str
     dateCreated: datetime
     dateModifiedAtLastProvider: Optional[datetime]
+    demarchesSimplifieesApplicationId: Optional[str]
     fieldsUpdated: List[str]
     iban: Optional[str]
     id: str

tests/routes/pro/get_offerer_test.py
@@ -3,6 +3,8 @@ import pytest
 import pcapi.core.offers.factories as offers_factories
 import pcapi.core.users.factories as users_factories
 from pcapi.model_creators.generic_creators import create_bank_information
+import pcapi.models.offerer as offerer_models
+from pcapi.utils.date import format_into_utc_date
 from pcapi.utils.human_ids import humanize
 
 from tests.conftest import TestClient
@@ -27,19 +29,67 @@ class Returns200:
     @pytest.mark.usefixtures("db_session")
     def when_user_has_rights_on_offerer(self, app):
         # given
-        pro = users_factories.UserFactory(isBeneficiary=False)
-        venue = offers_factories.VenueFactory()
-        offers_factories.UserOffererFactory(user=pro, offerer=venue.managingOfferer)
 
-        create_bank_information(venue=venue)
+        pro = users_factories.UserFactory(isBeneficiary=False)
+        offerer = offers_factories.OffererFactory()
+        offers_factories.UserOffererFactory(user=pro, offerer=offerer)
+        venue = offers_factories.VenueFactory(managingOfferer=offerer)
+
+        offerer_bank_information = create_bank_information(offerer=offerer)
+        venue_bank_information = create_bank_information(venue=venue, application_id=2)
+        offerer = offerer_models.Offerer.query.filter_by(id=offerer.id).first()
 
         # when
-        response = (
-            TestClient(app.test_client()).with_auth(pro.email).get(f"/offerers/{humanize(venue.managingOfferer.id)}")
-        )
+        response = TestClient(app.test_client()).with_auth(pro.email).get(f"/offerers/{humanize(offerer.id)}")
+
+        expected_serialized_offerer = {
+            "address": offerer.address,
+            "bic": offerer_bank_information.bic,
+            "iban": offerer_bank_information.iban,
+            "city": offerer.city,
+            "dateCreated": format_into_utc_date(offerer.dateCreated),
+            "dateModifiedAtLastProvider": format_into_utc_date(offerer.dateModifiedAtLastProvider),
+            "demarchesSimplifieesApplicationId": str(offerer.demarchesSimplifieesApplicationId),
+            "fieldsUpdated": offerer.fieldsUpdated,
+            "id": humanize(offerer.id),
+            "idAtProviders": offerer.idAtProviders,
+            "isValidated": offerer.isValidated,
+            "lastProviderId": offerer.lastProviderId,
+            "managedVenues": [
+                {
+                    "bic": venue_bank_information.bic,
+                    "iban": venue_bank_information.iban,
+                    "address": offererVenue.address,
+                    "bookingEmail": offererVenue.bookingEmail,
+                    "city": offererVenue.city,
+                    "comment": offererVenue.comment,
+                    "dateCreated": format_into_utc_date(offererVenue.dateCreated),
+                    "dateModifiedAtLastProvider": format_into_utc_date(offererVenue.dateModifiedAtLastProvider),
+                    "departementCode": offererVenue.departementCode,
+                    "id": humanize(offererVenue.id),
+                    "idAtProviders": offererVenue.idAtProviders,
+                    "isValidated": offererVenue.isValidated,
+                    "isVirtual": offererVenue.isVirtual,
+                    "lastProviderId": offererVenue.lastProviderId,
+                    "latitude": float(offererVenue.latitude),
+                    "longitude": float(offererVenue.longitude),
+                    "managingOffererId": humanize(offererVenue.managingOffererId),
+                    "name": offererVenue.name,
+                    "nOffers": offererVenue.nOffers,
+                    "postalCode": offererVenue.postalCode,
+                    "publicName": offererVenue.publicName,
+                    "siret": offererVenue.siret,
+                    "venueLabelId": humanize(offererVenue.venueLabelId),
+                    "venueTypeId": humanize(offererVenue.venueTypeId),
+                }
+                for offererVenue in offerer.managedVenues
+            ],
+            "name": offerer.name,
+            "nOffers": offerer.nOffers,
+            "postalCode": offerer.postalCode,
+            "siren": offerer.siren,
+        }
 
         # then
         assert response.status_code == 200
-        response_json = response.json
-        assert "bic" in response_json["managedVenues"][0]
-        assert "iban" in response_json["managedVenues"][0]
+        assert response.json == expected_serialized_offerer