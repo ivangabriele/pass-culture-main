ivangabriele/pass-culture-main

commit 65c6b727b036eb15c11b24f79bc2e45de31b9b1e

(PC-9311) admin: Fix XSS vulnerabilities in offer validation view
Also, use `Markup.format()` with explicitly escaped arguments
everywhere. This is not strictly needed (since `Markup.format()`
already escapes arguments), but this could help us not to introduce a
similar vulnerability by copy-pasting code and then trying to be
clever with an f-string.

Credit goes to Aethlios who responsibly disclosed this vulnerability.

src/pcapi/admin/custom_views/mixins/suspension_mixin.py
@@ -44,7 +44,11 @@ def _action_links(view, context, model, name):
         url = url_for(".unsuspend_user_view")
         text = "Réactiver&hellip;"
 
-    return Markup(f'<a href="{url}?user_id={model.id}">{text}</a>')
+    return Markup('<a href="{url}?user_id={model_id}">{text}</a>').format(
+        url=url,
+        model_id=model.id,
+        text=text,
+    )
 
 
 class SuspensionMixin:

src/pcapi/admin/custom_views/offer_view.py
@@ -14,6 +14,7 @@ from flask_admin.helpers import get_form_data
 from flask_admin.helpers import is_form_submitted
 from flask_login import current_user
 from markupsafe import Markup
+from markupsafe import escape
 from sqlalchemy import func
 from sqlalchemy.orm import query
 from werkzeug import Response
@@ -190,37 +191,29 @@ def _offerer_url(offerer_id: int) -> str:
 
 def _pro_offer_link(view, context, model, name) -> Markup:
     url = _pro_offer_url(model.id)
-    text = "Offre PC"
-
-    return Markup(f'<a href="{url}" target="_blank" rel="noopener noreferrer">{text}</a>')
+    return Markup('<a href="{}" target="_blank" rel="noopener noreferrer">Offre PC</a>').format(escape(url))
 
 
 def _related_offers_link(view, context, model, name) -> Markup:
     url = url_for("offer_for_venue.index", id=model.venue.id)
-    text = "Offres associées"
-
-    return Markup(f'<a href="{url}">{text}</a>')
+    return Markup('<a href="{}">Offres associées</a>').format(escape(url))
 
 
 def _metabase_offer_link(view, context, model, name) -> Markup:
     url = _metabase_offer_url(model.id)
-    text = "Offre"
-
-    return Markup(f'<a href="{url}" target="_blank" rel="noopener noreferrer">{text}</a>')
+    return Markup('<a href="{}" target="_blank" rel="noopener noreferrer">Offre</a>').format(escape(url))
 
 
 def _offerer_link(view, context, model, name) -> Markup:
     url = _offerer_url(model.venue.managingOffererId)
-    text = model.venue.managingOfferer.name
-
-    return Markup(f'<a href="{url}" target="_blank" rel="noopener noreferrer">{text}</a>')
+    link = Markup('<a href="{url}" target="_blank" rel="noopener noreferrer">{name}</a>')
+    return link.format(url=escape(url), name=escape(model.venue.managingOfferer.name))
 
 
 def _venue_link(view, context, model, name) -> Markup:
     url = _venue_url(model.venue)
-    text = model.venue.publicName or model.venue.name
-
-    return Markup(f'<a href="{url}" target="_blank" rel="noopener noreferrer">{text}</a>')
+    link = Markup('<a href="{url}" target="_blank" rel="noopener noreferrer">{name}</a>')
+    return link.format(url=escape(url), name=escape(model.venue.publicName or model.venue.name))
 
 
 def _compute_score(view, context, model, name) -> float:

src/pcapi/admin/custom_views/venue_provider_view.py
@@ -6,6 +6,7 @@ from flask import request
 from flask import url_for
 from flask_admin.helpers import get_form_data
 from markupsafe import Markup
+from markupsafe import escape
 from sqlalchemy.orm import query
 from wtforms import BooleanField
 from wtforms import DecimalField
@@ -25,9 +26,7 @@ from pcapi.utils.human_ids import humanize
 
 def _venue_link(view, context, model, name) -> Markup:
     url = url_for("venue.index_view", id=model.venueId)
-    text = "Lieu associé"
-
-    return Markup(f'<a href="{url}">{text}</a>')
+    return Markup('<a href="{}">Lieu associé</a>').format(escape(url))
 
 
 class VenueProviderView(BaseAdminView):

src/pcapi/admin/custom_views/venue_view.py
@@ -3,6 +3,7 @@ from typing import Union
 from flask import request
 from flask import url_for
 from markupsafe import Markup
+from markupsafe import escape
 from sqlalchemy.orm import query
 from wtforms import Form
 
@@ -13,18 +14,14 @@ from pcapi.core.offers.api import update_offer_and_stock_id_at_providers
 
 def _offers_link(view, context, model, name) -> Markup:
     url = url_for("offer_for_venue.index", id=model.id)
-    text = "Offres associées"
-
-    return Markup(f'<a href="{url}">{text}</a>')
+    return Markup('<a href="{}">Offres associées</a>').format(escape(url))
 
 
 def _get_venue_provider_link(view, context, model, name) -> Union[Markup, None]:
-
     if not model.venueProviders:
         return None
-
     url = url_for("venue_providers.index_view", id=model.id)
-    return Markup(f'<a href="{url}">Voir</a>')
+    return Markup('<a href="{}">Voir</a>').format(url)
 
 
 class VenueView(BaseAdminView):

commit e77272fda63f9ee2db955134fc3bfa2cf045138d

(PC-9311) admin: Fix over-escaping of suspend/reactivate link in user lists
The text was escaped by `Markup.format()`, which caused this to be
displayed in the browser:

    Suspendre&hellip;

The bug was introduced in 65c6b727b036eb15c11b24f79bc2e45de31b9b1e.

src/pcapi/admin/custom_views/mixins/suspension_mixin.py
@@ -39,10 +39,10 @@ def _action_links(view, context, model, name):
 
     if model.isActive:
         url = url_for(".suspend_user_view")
-        text = "Suspendre&hellip;"
+        text = "Suspendre…"
     else:
         url = url_for(".unsuspend_user_view")
-        text = "Réactiver&hellip;"
+        text = "Réactiver…"
 
     return Markup('<a href="{url}?user_id={model_id}">{text}</a>').format(
         url=url,