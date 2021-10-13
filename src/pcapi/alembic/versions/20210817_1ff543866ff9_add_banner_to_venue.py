noam74/pass-culture-api

(PC-10265) add image banner to venue
+ add two columns to Venue: one for the banner url, one the banner
  metadata (on might need the uploader id, the filename, and other
  information that might that are not mandatory to show the banner that
  could change over time)
+ add a new route (PRO) to upload the banner

src/pcapi/alembic/versions/20210817_1ff543866ff9_add_banner_to_venue.py
@@ -1,0 +1,27 @@
+"""add_banner_to_venue
+
+Revision ID: 1ff543866ff9
+Revises: f65cb6d7ef9b
+Create Date: 2021-08-17 20:41:21.090763
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision = "1ff543866ff9"
+down_revision = "f65cb6d7ef9b"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("venue", sa.Column("bannerMeta", postgresql.JSONB(astext_type=sa.Text()), nullable=True))
+    op.add_column("venue", sa.Column("bannerUrl", sa.Text(), nullable=True))
+
+
+def downgrade():
+    op.drop_column("venue", "bannerUrl")
+    op.drop_column("venue", "bannerMeta")

tests/routes/pro/post_venue_test.py
@@ -1,14 +1,25 @@
+import io
+import pathlib
+import tempfile
+from unittest.mock import patch
+
 import pytest
 
+from pcapi import settings
 import pcapi.core.offerers.factories as offerers_factories
 import pcapi.core.offers.factories as offers_factories
+from pcapi.core.testing import override_settings
 from pcapi.core.users.factories import ProFactory
 from pcapi.models import Venue
 from pcapi.repository import repository
 from pcapi.utils.human_ids import dehumanize
 from pcapi.utils.human_ids import humanize
 
+import tests
 from tests.conftest import TestClient
+
+
+IMAGES_DIR = pathlib.Path(tests.__path__[0]) / "files"
 
 
 @pytest.mark.usefixtures("db_session")
@@ -171,3 +182,41 @@ def test_should_return_403_when_user_is_not_managing_offerer_create_venue(app):
 
     assert response.status_code == 403
     assert response.json["global"] == ["Vous n'avez pas les droits d'accès suffisant pour accéder à cette information."]
+
+
+@pytest.mark.usefixtures("db_session")
+class VenueBannerTest:
+    @patch("pcapi.core.object_storage.backends.local.LocalBackend.local_dir")
+    def test_upload_image(self, mock_local_dir, app, client):
+        """
+        Check that the image upload works for a legit file (size and type):
+            * API returns a 204 status code
+            * the file has been saved to disk
+            * venue's banner information have been updated
+        """
+        user_offerer = offers_factories.UserOffererFactory()
+        venue = offers_factories.VenueFactory(managingOfferer=user_offerer.offerer)
+
+        image_content = (IMAGES_DIR / "mouette_landscape.jpg").read_bytes()
+        file = {"banner": (io.BytesIO(image_content), "jerome_le_banner.jpg")}
+
+        client = client.with_auth(email=user_offerer.user.email)
+        url = f"/venues/{humanize(venue.id)}/banner"
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            with override_settings(OBJECT_STORAGE_URL=tmpdirname):
+                settings.OBJECT_STORAGE_URL = tmpdirname
+                mock_local_dir.return_value = pathlib.Path(tmpdirname) / settings.VENUE_BANNER_BUCKET_NAME
+
+                response = client.post(url, files=file)
+                assert response.status_code == 204
+
+                venue = Venue.query.get(venue.id)
+                with open(venue.bannerUrl, mode="rb") as f:
+                    assert f.read() == image_content
+
+                assert venue.bannerMeta == {
+                    "content_type": "jpeg",
+                    "file_name": "jerome_le_banner.jpg",
+                    "author_id": user_offerer.user.id,
+                }

ivangabriele/pass-culture-main

src/pcapi/alembic/versions/20210817_1ff543866ff9_add_banner_to_venue.py
@@ -1,0 +1,27 @@
+"""add_banner_to_venue
+
+Revision ID: 1ff543866ff9
+Revises: f65cb6d7ef9b
+Create Date: 2021-08-17 20:41:21.090763
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision = "1ff543866ff9"
+down_revision = "f9bf2692bd95"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("venue", sa.Column("bannerMeta", postgresql.JSONB(astext_type=sa.Text()), nullable=True))
+    op.add_column("venue", sa.Column("bannerUrl", sa.Text(), nullable=True))
+
+
+def downgrade():
+    op.drop_column("venue", "bannerUrl")
+    op.drop_column("venue", "bannerMeta")

tests/routes/pro/post_venue_test.py
@@ -1,13 +1,24 @@
+import io
+import pathlib
+import tempfile
+from unittest.mock import patch
+
 import pytest
 
+from pcapi import settings
 import pcapi.core.offerers.factories as offerers_factories
 import pcapi.core.offers.factories as offers_factories
+from pcapi.core.testing import override_settings
 from pcapi.core.users.factories import ProFactory
 from pcapi.models import Venue
 from pcapi.utils.human_ids import dehumanize
 from pcapi.utils.human_ids import humanize
 
+import tests
 from tests.conftest import TestClient
+
+
+IMAGES_DIR = pathlib.Path(tests.__path__[0]) / "files"
 
 
 def create_valid_venue_data(user=None):
@@ -173,3 +184,41 @@ def test_create_venue_malformed(app, client, data, key):
 
     assert response.status_code == 400
     assert key in response.json
+
+
+@pytest.mark.usefixtures("db_session")
+class VenueBannerTest:
+    @patch("pcapi.core.object_storage.backends.local.LocalBackend.local_dir")
+    def test_upload_image(self, mock_local_dir, app, client):
+        """
+        Check that the image upload works for a legit file (size and type):
+            * API returns a 204 status code
+            * the file has been saved to disk
+            * venue's banner information have been updated
+        """
+        user_offerer = offers_factories.UserOffererFactory()
+        venue = offers_factories.VenueFactory(managingOfferer=user_offerer.offerer)
+
+        image_content = (IMAGES_DIR / "mouette_landscape.jpg").read_bytes()
+        file = {"banner": (io.BytesIO(image_content), "jerome_le_banner.jpg")}
+
+        client = client.with_session_auth(email=user_offerer.user.email)
+        url = f"/venues/{humanize(venue.id)}/banner"
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            with override_settings(OBJECT_STORAGE_URL=tmpdirname):
+                settings.OBJECT_STORAGE_URL = tmpdirname
+                mock_local_dir.return_value = pathlib.Path(tmpdirname) / settings.BASE_BUCKET_NAME
+
+                response = client.post(url, files=file)
+                assert response.status_code == 204
+
+                venue = Venue.query.get(venue.id)
+                with open(venue.bannerUrl, mode="rb") as f:
+                    assert f.read() == image_content
+
+                assert venue.bannerMeta == {
+                    "content_type": "jpeg",
+                    "file_name": "jerome_le_banner.jpg",
+                    "author_id": user_offerer.user.id,
+                }