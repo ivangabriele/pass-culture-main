d0b9db106471c4c858918c41a8e0ca90ab4c74fd
rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>
2025-06-26 14:02:31 +0200

    Revert "(BSR)[BO] test: (upgrade libs) fix expected num queries when rollback()"
    This reverts commit 8a7b6acadd76d4e8f95c15c9547e474f40e4e4b5.

--- api/tests/routes/backoffice/chronicles_test.py
+++ api/tests/routes/backoffice/chronicles_test.py
@@ -371,11 +371,10 @@ class PublishChronicleTest(PostEndpointHelper):
     # session
     # current user
     # get chronicle
-    expected_num_queries_base = 3
     # update chronicle
     # reload chronicle
     # ListChroniclesTest.expected_num_queries (follow redirect)
-    expected_num_queries = expected_num_queries_base + 2 + ListChroniclesTest.expected_num_queries
+    expected_num_queries = 5 + ListChroniclesTest.expected_num_queries
 
     def test_publish_chronicle(self, authenticated_client, legit_user):
         chronicle = chronicles_factories.ChronicleFactory(
@@ -400,7 +399,7 @@ class PublishChronicleTest(PostEndpointHelper):
     def test_publish_chronicle_does_not_exist(self, authenticated_client):
         response = self.post_to_endpoint(
             follow_redirects=True,
-            expected_num_queries=self.expected_num_queries_base,
+            expected_num_queries=self.expected_num_queries - (1 + ListChroniclesTest.expected_num_queries),
             chronicle_id=0,
             client=authenticated_client,
         )
@@ -414,11 +413,10 @@ class UnpublishChronicleTest(PostEndpointHelper):
     # session
     # current user
     # get chronicle
-    expected_num_queries_base = 3
     # update chronicle
     # reload chronicle
     # ListChroniclesTest.expected_num_queries (follow redirect)
-    expected_num_queries = expected_num_queries_base + 2 + ListChroniclesTest.expected_num_queries
+    expected_num_queries = 5 + ListChroniclesTest.expected_num_queries
 
     def test_unpublish_chronicle(self, authenticated_client, legit_user):
         chronicle = chronicles_factories.ChronicleFactory(
@@ -443,7 +441,7 @@ class UnpublishChronicleTest(PostEndpointHelper):
     def test_unpublish_chronicle_does_not_exist(self, authenticated_client):
         response = self.post_to_endpoint(
             follow_redirects=True,
-            expected_num_queries=self.expected_num_queries_base,
+            expected_num_queries=self.expected_num_queries - (1 + ListChroniclesTest.expected_num_queries),
             chronicle_id=0,
             client=authenticated_client,
         )
@@ -674,7 +672,7 @@ class DetachProductTest(PostEndpointHelper):
 
         response = self.post_to_endpoint(
             follow_redirects=True,
-            expected_num_queries=self.expected_num_queries,
+            expected_num_queries=self.expected_num_queries + 1,  # rollback on error
             chronicle_id=chronicle.id,
             product_id=product.id,
             client=authenticated_client,
