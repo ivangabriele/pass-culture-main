ivangabriele/pass-culture-main

commit 62eb886cb9c54d315ea4d0d5ef823d1dea3b6be9

(BSR)[API] bump: upgrade flask, flask_login, flask-sqlalchemy, sqlalchelmy, werkzeug, clickhouse-sqlalchemy
Co-authored-by: rpaoloni-pass <185350975+rpaoloni-pass@users.noreply.github.com>

api/src/pcapi/core/users/ds.py
@@ -130,8 +130,8 @@ def sync_user_account_update_requests(
     logger.info("[DS] Started processing User Update Account procedure %s", procedure_number)
 
     # Fetch instructors' User IDs only once
-    user_id_by_email = dict(
-        db.session.query(users_models.User.email, users_models.User.id)
+    user_id_by_email: dict[str, int] = dict(
+        db.session.query(users_models.User.email, users_models.User.id)  # type: ignore [arg-type]
         .join(users_models.User.backoffice_profile)
         .filter(perm_models.BackOfficeUserProfile.dsInstructorId.is_not(None))
         .all()
@@ -529,6 +529,7 @@ def update_state(
 
     for key, value in _get_updated_data(node).items():
         setattr(user_request, key, value)
+    assert instructor  # helps mypy
     user_request.lastInstructor = instructor
     db.session.add(user_request)
     db.session.flush()
@@ -568,9 +569,11 @@ def send_user_message_with_correction(
     )
     update_request.lastInstructor = instructor
     update_request.status = dms_models.GraphQLApplicationStates.draft
-    update_request.flags = [
+    flags: list[users_models.UserAccountUpdateFlag] = [
         flag for flag in update_request.flags if flag != users_models.UserAccountUpdateFlag.CORRECTION_RESOLVED
-    ] + [users_models.UserAccountUpdateFlag.WAITING_FOR_CORRECTION]
+    ]
+    flags.append(users_models.UserAccountUpdateFlag.WAITING_FOR_CORRECTION)
+    update_request.flags = flags
     update_request.dateLastStatusUpdate = _from_ds_date(node["dossierEnvoyerMessage"]["message"]["createdAt"])
     update_request.dateLastInstructorMessage = _from_ds_date(node["dossierEnvoyerMessage"]["message"]["createdAt"])
 

api/src/pcapi/connectors/dms/models.py
@@ -4,19 +4,21 @@ import enum
 import pydantic.v1 as pydantic_v1
 import pytz
 import sqlalchemy as sa
+from sqlalchemy import orm as sa_orm
 from sqlalchemy.dialects import postgresql
 
-from pcapi.models import Base
 from pcapi.models import Model
 from pcapi.models.pc_object import PcObject
 
 
-class LatestDmsImport(PcObject, Base, Model):
+class LatestDmsImport(PcObject, Model):
     __tablename__ = "latest_dms_import"
-    procedureId = sa.Column(sa.Integer, nullable=False)
-    latestImportDatetime: datetime.datetime = sa.Column(sa.DateTime, nullable=False)
-    isProcessing = sa.Column(sa.Boolean, nullable=False)
-    processedApplications: list[int] = sa.Column(postgresql.ARRAY(sa.Integer), nullable=False, default=[])
+    procedureId = sa_orm.mapped_column(sa.Integer, nullable=False)
+    latestImportDatetime: sa_orm.Mapped[datetime.datetime] = sa_orm.mapped_column(sa.DateTime, nullable=False)
+    isProcessing = sa_orm.mapped_column(sa.Boolean, nullable=False)
+    processedApplications: sa_orm.Mapped[list[int]] = sa_orm.mapped_column(
+        postgresql.ARRAY(sa.Integer), nullable=False, default=[]
+    )
 
 
 def parse_dms_datetime(value: datetime.datetime | None) -> datetime.datetime | None:

api/src/pcapi/core/achievements/models.py
@@ -6,7 +6,6 @@ import sqlalchemy as sa
 import sqlalchemy.orm as sa_orm
 
 import pcapi.utils.db as db_utils
-from pcapi.models import Base
 from pcapi.models import Model
 from pcapi.models.pc_object import PcObject
 
@@ -28,20 +27,24 @@ class AchievementEnum(enum.Enum):
     FIRST_ART_LESSON_BOOKING = "FIRST_ART_LESSON_BOOKING"
 
 
-class Achievement(PcObject, Base, Model):
+class Achievement(PcObject, Model):
     __tablename__ = "achievement"
 
-    userId: int = sa.Column(sa.BigInteger, sa.ForeignKey("user.id"), index=True, nullable=False)
+    userId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("user.id"), index=True, nullable=False
+    )
     user: sa_orm.Mapped["User"] = sa_orm.relationship("User", foreign_keys=[userId], backref="achievements")
 
-    bookingId: int = sa.Column(sa.BigInteger, sa.ForeignKey("booking.id"), nullable=False, index=True)
+    bookingId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("booking.id"), nullable=False, index=True
+    )
     booking: sa_orm.Mapped["Booking"] = sa_orm.relationship("Booking", foreign_keys=[bookingId], backref="achievements")
 
-    name: AchievementEnum = sa.Column(db_utils.MagicEnum(AchievementEnum), nullable=False)
-    unlockedDate: datetime = sa.Column(
+    name: sa_orm.Mapped[AchievementEnum] = sa_orm.mapped_column(db_utils.MagicEnum(AchievementEnum), nullable=False)
+    unlockedDate: sa_orm.Mapped[datetime] = sa_orm.mapped_column(
         sa.DateTime, nullable=False, default=datetime.utcnow, server_default=sa.func.now()
     )
     # For when the user has seen the achievement success modal in the native app:
-    seenDate: datetime = sa.Column(sa.DateTime, nullable=True)
+    seenDate: sa_orm.Mapped[datetime] = sa_orm.mapped_column(sa.DateTime, nullable=True)
 
     __table_args__ = (sa.UniqueConstraint("userId", "name", name="user_achievement_unique"),)

api/src/pcapi/core/artist/models.py
@@ -5,7 +5,6 @@ import uuid
 import sqlalchemy as sa
 import sqlalchemy.orm as sa_orm
 
-from pcapi.models import Base
 from pcapi.models import Model
 from pcapi.models.pc_object import PcObject
 from pcapi.utils.db import MagicEnum
@@ -25,43 +24,56 @@ class ArtistType(enum.Enum):
     PERFORMER = "performer"
 
 
-class ArtistProductLink(PcObject, Base, Model):
+class ArtistProductLink(PcObject, Model):
     __tablename__ = "artist_product_link"
 
-    artist_id = sa.Column(sa.Text, sa.ForeignKey("artist.id", ondelete="CASCADE"), nullable=False, index=True)
-    product_id = sa.Column(sa.BigInteger, sa.ForeignKey("product.id", ondelete="CASCADE"), nullable=False, index=True)
-    artist_type = sa.Column(MagicEnum(ArtistType))
-    date_created = sa.Column(sa.DateTime, nullable=False, server_default=sa.func.now())
-    date_modified = sa.Column(sa.DateTime, nullable=True, onupdate=sa.func.now())
+    artist_id = sa_orm.mapped_column(
+        sa.Text, sa.ForeignKey("artist.id", ondelete="CASCADE"), nullable=False, index=True
+    )
+    product_id = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("product.id", ondelete="CASCADE"), nullable=False, index=True
+    )
+    artist_type = sa_orm.mapped_column(MagicEnum(ArtistType))
+    date_created = sa_orm.mapped_column(sa.DateTime, nullable=False, server_default=sa.func.now())
+    date_modified = sa_orm.mapped_column(sa.DateTime, nullable=True, onupdate=sa.func.now())
 
 
-class Artist(PcObject, Base, Model):
+class Artist(PcObject, Model):
     __tablename__ = "artist"
-    id = sa.Column(sa.Text, primary_key=True, nullable=False, default=lambda _: str(uuid.uuid4()))
-    name = sa.Column(sa.Text, nullable=False, index=True)
-    computed_image = sa.Column(sa.Text)
-    description = sa.Column(sa.Text)
-    image = sa.Column(sa.Text)
-    image_author = sa.Column(sa.Text)
-    image_license = sa.Column(sa.Text)
-    image_license_url = sa.Column(sa.Text)
-    date_created = sa.Column(sa.DateTime, nullable=False, server_default=sa.func.now())
-    date_modified = sa.Column(sa.DateTime, nullable=True, onupdate=sa.func.now())
-    is_blacklisted = sa.Column(sa.Boolean, nullable=False, server_default=sa.false(), default=False)
+    id = sa_orm.mapped_column(sa.Text, primary_key=True, nullable=False, default=lambda _: str(uuid.uuid4()))
+    name = sa_orm.mapped_column(sa.Text, nullable=False, index=True)
+    computed_image = sa_orm.mapped_column(sa.Text)
+    description = sa_orm.mapped_column(sa.Text)
+    image = sa_orm.mapped_column(sa.Text)
+    image_author = sa_orm.mapped_column(sa.Text)
+    image_license = sa_orm.mapped_column(sa.Text)
+    image_license_url = sa_orm.mapped_column(sa.Text)
+    date_created = sa_orm.mapped_column(sa.DateTime, nullable=False, server_default=sa.func.now())
+    date_modified = sa_orm.mapped_column(sa.DateTime, nullable=True, onupdate=sa.func.now())
+    is_blacklisted = sa_orm.mapped_column(sa.Boolean, nullable=False, server_default=sa.false(), default=False)
 
     products: sa_orm.Mapped[list["Product"]] = sa_orm.relationship(
         "Product", backref="artists", secondary=ArtistProductLink.__table__
     )
     aliases: sa_orm.Mapped[list["ArtistAlias"]] = sa_orm.relationship("ArtistAlias", backref="artist")
 
-    sa.Index("ix_artist_trgm_unaccent_name", sa.func.immutable_unaccent("name"), postgresql_using="gin")
+    __table_args__ = (
+        sa.Index(
+            "ix_artist_trgm_unaccent_name",
+            sa.func.immutable_unaccent("name"),
+            postgresql_using="gin",
+            postgresql_ops={
+                "description": "gin_trgm_ops",
+            },
+        ),
+    )
 
     @property
     def thumbUrl(self) -> str | None:
         return self.image or self.computed_image
 
 
-class ArtistAlias(PcObject, Base, Model):
+class ArtistAlias(PcObject, Model):
     """
     The data in this table is used by the data team
     to reconcile artists across different sources (e.g. Wikidata)
@@ -70,15 +82,23 @@ class ArtistAlias(PcObject, Base, Model):
     """
 
     __tablename__ = "artist_alias"
-    artist_id = sa.Column(sa.Text, sa.ForeignKey("artist.id", ondelete="CASCADE"), nullable=False, index=True)
-    artist_alias_name = sa.Column(sa.Text)
-    artist_cluster_id = sa.Column(sa.Text)
-    artist_type = sa.Column(MagicEnum(ArtistType))
-    artist_wiki_data_id = sa.Column(sa.Text)
-    offer_category_id = sa.Column(sa.Text)
-    date_created = sa.Column(sa.DateTime, nullable=False, server_default=sa.func.now())
-    date_modified = sa.Column(sa.DateTime, nullable=True, onupdate=sa.func.now())
-
-    sa.Index(
-        "ix_artist_alias_trgm_unaccent_name", sa.func.immutable_unaccent("artist_alias_name"), postgresql_using="gin"
+    artist_id = sa_orm.mapped_column(
+        sa.Text, sa.ForeignKey("artist.id", ondelete="CASCADE"), nullable=False, index=True
+    )
+    artist_alias_name = sa_orm.mapped_column(sa.Text)
+    artist_cluster_id = sa_orm.mapped_column(sa.Text)
+    artist_type = sa_orm.mapped_column(MagicEnum(ArtistType))
+    artist_wiki_data_id = sa_orm.mapped_column(sa.Text)
+    offer_category_id = sa_orm.mapped_column(sa.Text)
+    date_created = sa_orm.mapped_column(sa.DateTime, nullable=False, server_default=sa.func.now())
+    date_modified = sa_orm.mapped_column(sa.DateTime, nullable=True, onupdate=sa.func.now())
+    __table_args__ = (
+        sa.Index(
+            "ix_artist_alias_trgm_unaccent_name",
+            sa.func.immutable_unaccent("artist_alias_name"),
+            postgresql_using="gin",
+            postgresql_ops={
+                "description": "gin_trgm_ops",
+            },
+        ),
     )

api/src/pcapi/core/bookings/models.py
@@ -23,7 +23,6 @@ from pcapi.core.categories import subcategories
 from pcapi.core.offers import models as offers_models
 from pcapi.core.reactions.models import ReactionTypeEnum
 from pcapi.core.users import models as users_models
-from pcapi.models import Base
 from pcapi.models import Model
 from pcapi.models.pc_object import PcObject
 from pcapi.utils.db import MagicEnum
@@ -98,52 +97,62 @@ class BookingRecreditType(enum.Enum):
     RECREDIT_18 = "RECREDIT_18"
 
 
-class ExternalBooking(PcObject, Base, Model):
+class ExternalBooking(PcObject, Model):
     __tablename__ = "external_booking"
-    bookingId: int = sa.Column(sa.BigInteger, sa.ForeignKey("booking.id"), index=True, nullable=False)
+    bookingId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("booking.id"), index=True, nullable=False
+    )
 
     booking: sa_orm.Mapped["Booking"] = sa_orm.relationship(
         "Booking", foreign_keys=[bookingId], backref="externalBookings"
     )
 
-    barcode: str = sa.Column(sa.String, nullable=False)
+    barcode: sa_orm.Mapped[str] = sa_orm.mapped_column(sa.String, nullable=False)
 
-    seat = sa.Column(sa.String)
+    seat = sa_orm.mapped_column(sa.String)
 
-    additional_information: dict | None = sa.Column(postgresql.JSONB)
+    additional_information: sa_orm.Mapped[dict | None] = sa_orm.mapped_column(postgresql.JSONB)
 
 
-class Booking(PcObject, Base, Model):
+class Booking(PcObject, Model):
     __tablename__ = "booking"
 
-    dateCreated: datetime = sa.Column(sa.DateTime, nullable=False, default=datetime.utcnow)
+    dateCreated: sa_orm.Mapped[datetime] = sa_orm.mapped_column(sa.DateTime, nullable=False, default=datetime.utcnow)
     sa.Index("ix_booking_date_created", dateCreated)
 
-    dateUsed: datetime | None = sa.Column(sa.DateTime, nullable=True, index=True)
+    dateUsed: sa_orm.Mapped[datetime | None] = sa_orm.mapped_column(sa.DateTime, nullable=True, index=True)
 
-    stockId: int = sa.Column(sa.BigInteger, sa.ForeignKey("stock.id"), index=True, nullable=False)
+    stockId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("stock.id"), index=True, nullable=False
+    )
 
     stock: sa_orm.Mapped["offers_models.Stock"] = sa_orm.relationship(
         "Stock", foreign_keys=[stockId], backref="bookings"
     )
 
-    venueId: int = sa.Column(sa.BigInteger, sa.ForeignKey("venue.id"), index=True, nullable=False)
+    venueId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("venue.id"), index=True, nullable=False
+    )
 
     venue: sa_orm.Mapped["offerers_models.Venue"] = sa_orm.relationship(
         "Venue", foreign_keys=[venueId], backref="bookings"
     )
 
-    offererId: int = sa.Column(sa.BigInteger, sa.ForeignKey("offerer.id"), index=True, nullable=False)
+    offererId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("offerer.id"), index=True, nullable=False
+    )
 
     offerer: sa_orm.Mapped["offerers_models.Offerer"] = sa_orm.relationship(
         "Offerer", foreign_keys=[offererId], backref="bookings"
     )
 
-    quantity: int = sa.Column(sa.Integer, nullable=False, default=1)
+    quantity: sa_orm.Mapped[int] = sa_orm.mapped_column(sa.Integer, nullable=False, default=1)
 
-    token: str = sa.Column(sa.String(6), unique=True, nullable=False)
+    token: sa_orm.Mapped[str] = sa_orm.mapped_column(sa.String(6), unique=True, nullable=False)
 
-    userId: int = sa.Column(sa.BigInteger, sa.ForeignKey("user.id"), index=True, nullable=False)
+    userId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("user.id"), index=True, nullable=False
+    )
 
     activationCode: sa_orm.Mapped["offers_models.ActivationCode"] = sa_orm.relationship(
         "ActivationCode", uselist=False, back_populates="booking"
@@ -153,17 +162,17 @@ class Booking(PcObject, Base, Model):
         "User", foreign_keys=[userId], backref="userBookings"
     )
 
-    amount: Decimal = sa.Column(sa.Numeric(10, 2), nullable=False)
+    amount: sa_orm.Mapped[Decimal] = sa_orm.mapped_column(sa.Numeric(10, 2), nullable=False)
 
-    priceCategoryLabel: str | None = sa.Column(sa.Text, nullable=True)
+    priceCategoryLabel: sa_orm.Mapped[str | None] = sa_orm.mapped_column(sa.Text, nullable=True)
 
-    cancellationDate: datetime | None = sa.Column(sa.DateTime, nullable=True)
+    cancellationDate: sa_orm.Mapped[datetime | None] = sa_orm.mapped_column(sa.DateTime, nullable=True)
 
-    displayAsEnded = sa.Column(sa.Boolean, nullable=True)
+    displayAsEnded = sa_orm.mapped_column(sa.Boolean, nullable=True)
 
-    cancellationLimitDate = sa.Column(sa.DateTime, nullable=True)
+    cancellationLimitDate = sa_orm.mapped_column(sa.DateTime, nullable=True)
 
-    cancellationReason = sa.Column(
+    cancellationReason = sa_orm.mapped_column(
         "cancellationReason",
         sa.Enum(
             BookingCancellationReasons,
@@ -177,27 +186,35 @@ class Booking(PcObject, Base, Model):
         postgresql_where=cancellationReason.is_not(None),
     )
 
-    cancellationUserId: int | None = sa.Column(sa.BigInteger, sa.ForeignKey("user.id"), nullable=True)
+    cancellationUserId: sa_orm.Mapped[int | None] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("user.id"), nullable=True
+    )
     cancellationUser: sa_orm.Mapped["users_models.User | None"] = sa_orm.relationship(
         "User", foreign_keys=[cancellationUserId]
     )
     # Index avoids timeout when any user is deleted (because of foreign key)
     sa.Index("ix_booking_cancellationUserId", cancellationUserId, postgresql_where=cancellationUserId.is_not(None))
 
-    status: BookingStatus = sa.Column(sa.Enum(BookingStatus), nullable=False, default=BookingStatus.CONFIRMED)
+    status: sa_orm.Mapped[BookingStatus] = sa_orm.mapped_column(
+        sa.Enum(BookingStatus), nullable=False, default=BookingStatus.CONFIRMED
+    )
     sa.Index("ix_booking_status", status)
 
-    validationAuthorType: BookingValidationAuthorType = sa.Column(sa.Enum(BookingValidationAuthorType), nullable=True)
+    validationAuthorType: sa_orm.Mapped[BookingValidationAuthorType] = sa_orm.mapped_column(
+        sa.Enum(BookingValidationAuthorType), nullable=True
+    )
 
-    reimbursementDate = sa.Column(sa.DateTime, nullable=True)
+    reimbursementDate = sa_orm.mapped_column(sa.DateTime, nullable=True)
 
-    depositId = sa.Column(sa.BigInteger, sa.ForeignKey("deposit.id"), index=True, nullable=True)
+    depositId = sa_orm.mapped_column(sa.BigInteger, sa.ForeignKey("deposit.id"), index=True, nullable=True)
 
     deposit: sa_orm.Mapped["finance_models.Deposit | None"] = sa_orm.relationship(
         "Deposit", foreign_keys=[depositId], back_populates="bookings"
     )
 
-    usedRecreditType: BookingRecreditType = sa.Column(MagicEnum(BookingRecreditType), nullable=True)
+    usedRecreditType: sa_orm.Mapped[BookingRecreditType] = sa_orm.mapped_column(
+        MagicEnum(BookingRecreditType), nullable=True
+    )
 
     fraudulentBookingTag: sa_orm.Mapped["FraudulentBookingTag"] = sa_orm.relationship(
         "FraudulentBookingTag", back_populates="booking", uselist=False
@@ -589,16 +606,18 @@ Booking.trig_update_cancellationDate_on_isCancelled_ddl = f"""
 sa.event.listen(Booking.__table__, "after_create", sa.DDL(Booking.trig_update_cancellationDate_on_isCancelled_ddl))
 
 
-class FraudulentBookingTag(PcObject, Base, Model):
+class FraudulentBookingTag(PcObject, Model):
     __tablename__ = "fraudulent_booking_tag"
-    dateCreated: datetime = sa.Column(sa.DateTime, nullable=False, default=datetime.utcnow)
+    dateCreated: sa_orm.Mapped[datetime] = sa_orm.mapped_column(sa.DateTime, nullable=False, default=datetime.utcnow)
 
-    bookingId: int = sa.Column(sa.BigInteger, sa.ForeignKey("booking.id"), index=True, nullable=False, unique=True)
+    bookingId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("booking.id"), index=True, nullable=False, unique=True
+    )
 
     booking: sa_orm.Mapped["Booking"] = sa_orm.relationship(
         "Booking", foreign_keys=[bookingId], back_populates="fraudulentBookingTag"
     )
 
-    authorId = sa.Column(sa.BigInteger, sa.ForeignKey("user.id"), index=True, nullable=False)
+    authorId = sa_orm.mapped_column(sa.BigInteger, sa.ForeignKey("user.id"), index=True, nullable=False)
 
     author: sa_orm.Mapped["users_models.User | None"] = sa_orm.relationship("User", foreign_keys=[authorId])

api/src/pcapi/core/chronicles/models.py
@@ -10,7 +10,6 @@ from sqlalchemy.sql.elements import BinaryExpression
 from pcapi.core.offers.models import Offer
 from pcapi.core.offers.models import Product
 from pcapi.core.users.models import User
-from pcapi.models import Base
 from pcapi.models import Model
 from pcapi.models.deactivable_mixin import DeactivableMixin
 from pcapi.models.pc_object import PcObject
@@ -20,12 +19,12 @@ from pcapi.utils import db as db_utils
 logger = logging.getLogger(__name__)
 
 
-class ProductChronicle(PcObject, Base, Model):
+class ProductChronicle(PcObject, Model):
     __tablename__ = "product_chronicle"
-    productId: int = sa.Column(
+    productId: sa_orm.Mapped[int] = sa_orm.mapped_column(
         sa.BigInteger, sa.ForeignKey("product.id", ondelete="CASCADE"), index=True, nullable=False
     )
-    chronicleId: int = sa.Column(
+    chronicleId: sa_orm.Mapped[int] = sa_orm.mapped_column(
         sa.BigInteger, sa.ForeignKey("chronicle.id", ondelete="CASCADE"), index=True, nullable=False
     )
 
@@ -38,10 +37,12 @@ class ProductChronicle(PcObject, Base, Model):
     )
 
 
-class OfferChronicle(PcObject, Base, Model):
+class OfferChronicle(PcObject, Model):
     __tablename__ = "offer_chronicle"
-    offerId: int = sa.Column(sa.BigInteger, sa.ForeignKey("offer.id", ondelete="CASCADE"), index=True, nullable=False)
-    chronicleId: int = sa.Column(
+    offerId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("offer.id", ondelete="CASCADE"), index=True, nullable=False
+    )
+    chronicleId: sa_orm.Mapped[int] = sa_orm.mapped_column(
         sa.BigInteger, sa.ForeignKey("chronicle.id", ondelete="CASCADE"), index=True, nullable=False
     )
 
@@ -65,22 +66,22 @@ class ChronicleClubType(enum.Enum):
     CINE_CLUB = "CINE"
 
 
-class Chronicle(PcObject, Base, Model, DeactivableMixin):
+class Chronicle(PcObject, Model, DeactivableMixin):
     __tablename__ = "chronicle"
-    age = sa.Column(sa.SmallInteger, nullable=True)
-    city = sa.Column(sa.Text(), nullable=True)
-    clubType = sa.Column(db_utils.MagicEnum(ChronicleClubType), nullable=False)
-    content = sa.Column(sa.Text, nullable=False)
-    dateCreated = sa.Column(sa.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    age = sa_orm.mapped_column(sa.SmallInteger, nullable=True)
+    city = sa_orm.mapped_column(sa.Text(), nullable=True)
+    clubType = sa_orm.mapped_column(db_utils.MagicEnum(ChronicleClubType), nullable=False)
+    content = sa_orm.mapped_column(sa.Text, nullable=False)
+    dateCreated = sa_orm.mapped_column(sa.DateTime, nullable=False, default=datetime.datetime.utcnow)
     # used to reconciliate data if the form changed on typeform
-    identifierChoiceId = sa.Column(sa.Text(), nullable=True)
-    email = sa.Column(sa.Text(), nullable=False)
-    firstName = sa.Column(sa.Text(), nullable=True)
-    externalId = sa.Column(sa.Text(), nullable=False, unique=True)
-    isIdentityDiffusible = sa.Column(
+    identifierChoiceId = sa_orm.mapped_column(sa.Text(), nullable=True)
+    email = sa_orm.mapped_column(sa.Text(), nullable=False)
+    firstName = sa_orm.mapped_column(sa.Text(), nullable=True)
+    externalId = sa_orm.mapped_column(sa.Text(), nullable=False, unique=True)
+    isIdentityDiffusible = sa_orm.mapped_column(
         sa.Boolean, nullable=False, server_default=sa.sql.expression.false(), default=False
     )
-    isSocialMediaDiffusible: bool = sa.Column(
+    isSocialMediaDiffusible: sa_orm.Mapped[bool] = sa_orm.mapped_column(
         sa.Boolean, nullable=False, server_default=sa.sql.expression.false(), default=False
     )
     products: sa_orm.Mapped[list["Product"]] = sa_orm.relationship(
@@ -89,20 +90,22 @@ class Chronicle(PcObject, Base, Model, DeactivableMixin):
     offers: sa_orm.Mapped[list["Offer"]] = sa_orm.relationship(
         "Offer", backref="chronicles", secondary=OfferChronicle.__table__
     )
-    productIdentifierType: ChronicleProductIdentifierType = sa.Column(
+    productIdentifierType: sa_orm.Mapped[ChronicleProductIdentifierType] = sa_orm.mapped_column(
         db_utils.MagicEnum(ChronicleProductIdentifierType), nullable=False
     )
-    productIdentifier = sa.Column(sa.Text(), nullable=False, index=True)
-    userId = sa.Column(sa.BigInteger, sa.ForeignKey("user.id", ondelete="SET NULL"), nullable=True, index=True)
+    productIdentifier = sa_orm.mapped_column(sa.Text(), nullable=False, index=True)
+    userId = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("user.id", ondelete="SET NULL"), nullable=True, index=True
+    )
     user: sa_orm.Mapped["User"] = sa_orm.relationship("User", foreign_keys=[userId], backref="chronicles")
 
-    __content_ts_vector__ = sa.Column(
+    _content_ts_vector = sa_orm.mapped_column(
         db_utils.TSVector(),
         sa.Computed("to_tsvector('french', content)", persisted=True),
         nullable=False,
         name="__content_ts_vector__",
     )
-    __table_args__ = (sa.Index("ix_chronicle_content___ts_vector__", __content_ts_vector__, postgresql_using="gin"),)
+    __table_args__ = (sa.Index("ix_chronicle_content___ts_vector__", _content_ts_vector, postgresql_using="gin"),)
 
     @hybrid_property
     def isPublished(self) -> bool:

api/src/pcapi/core/criteria/models.py
@@ -1,30 +1,29 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as sa_orm
 
-from pcapi.models import Base
 from pcapi.models import Model
 from pcapi.models.pc_object import PcObject
 
 
-class CriterionCategoryMapping(PcObject, Base, Model):
+class CriterionCategoryMapping(PcObject, Model):
     __tablename__ = "criterion_category_mapping"
 
-    criterionId: int = sa.Column(
+    criterionId: sa_orm.Mapped[int] = sa_orm.mapped_column(
         sa.BigInteger, sa.ForeignKey("criterion.id", ondelete="CASCADE"), index=True, nullable=False
     )
-    categoryId: int = sa.Column(
+    categoryId: sa_orm.Mapped[int] = sa_orm.mapped_column(
         sa.BigInteger, sa.ForeignKey("criterion_category.id", ondelete="CASCADE"), index=True, nullable=False
     )
 
     __table_args__ = (sa.UniqueConstraint("criterionId", "categoryId", name="unique_criterion_category"),)
 
 
-class Criterion(PcObject, Base, Model):
+class Criterion(PcObject, Model):
     __tablename__ = "criterion"
-    name: str = sa.Column(sa.String(140), nullable=False, unique=True)
-    description = sa.Column(sa.Text, nullable=True)
-    startDateTime = sa.Column(sa.DateTime, nullable=True)
-    endDateTime = sa.Column(sa.DateTime, nullable=True)
+    name: sa_orm.Mapped[str] = sa_orm.mapped_column(sa.String(140), nullable=False, unique=True)
+    description = sa_orm.mapped_column(sa.Text, nullable=True)
+    startDateTime = sa_orm.mapped_column(sa.DateTime, nullable=True)
+    endDateTime = sa_orm.mapped_column(sa.DateTime, nullable=True)
 
     categories: sa_orm.Mapped[list["CriterionCategory"]] = sa_orm.relationship(
         "CriterionCategory", secondary=CriterionCategoryMapping.__table__
@@ -34,10 +33,12 @@ class Criterion(PcObject, Base, Model):
         return self.name
 
 
-class VenueCriterion(PcObject, Base, Model):
+class VenueCriterion(PcObject, Model):
     __tablename__ = "venue_criterion"
-    venueId: int = sa.Column(sa.BigInteger, sa.ForeignKey("venue.id", ondelete="CASCADE"), index=True, nullable=False)
-    criterionId: int = sa.Column(
+    venueId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("venue.id", ondelete="CASCADE"), index=True, nullable=False
+    )
+    criterionId: sa_orm.Mapped[int] = sa_orm.mapped_column(
         sa.BigInteger, sa.ForeignKey("criterion.id", ondelete="CASCADE"), nullable=False, index=True
     )
 
@@ -50,10 +51,12 @@ class VenueCriterion(PcObject, Base, Model):
     )
 
 
-class OfferCriterion(PcObject, Base, Model):
+class OfferCriterion(PcObject, Model):
     __tablename__ = "offer_criterion"
-    offerId: int = sa.Column(sa.BigInteger, sa.ForeignKey("offer.id", ondelete="CASCADE"), index=True, nullable=False)
-    criterionId: int = sa.Column(
+    offerId: sa_orm.Mapped[int] = sa_orm.mapped_column(
+        sa.BigInteger, sa.ForeignKey("offer.id", ondelete="CASCADE"), index=True, nullable=False
+    )
+    criterionId: sa_orm.Mapped[int] = sa_orm.mapped_column(
         sa.BigInteger, sa.ForeignKey("criterion.id", ondelete="CASCADE"), index=True, nullable=False
     )
 
@@ -66,14 +69,14 @@ class OfferCriterion(PcObject, Base, Model):
     )
 
 
-class CriterionCategory(PcObject, Base, Model):
+class CriterionCategory(PcObject, Model):
     """
     Criterion categories used for partners counting, reporting, etc.
     """
 
     __tablename__ = "criterion_category"
 
-    label: str = sa.Column(sa.String(140), nullable=False, unique=True)
+    label: sa_orm.Mapped[str] = sa_orm.mapped_column(sa.String(140), nullable=False, unique=True)
 
     def __str__(self) -> str:
         return self.label

commit d89e0e134154ca674b1186fcf3e11ed2e7b03818

(PC-38230)[API] fix: AssertionError in DS sync for user account update requests
Regression from commit 62eb886c

api/src/pcapi/core/users/ds.py
@@ -529,7 +529,6 @@ def update_state(
 
     for key, value in _get_updated_data(node).items():
         setattr(user_request, key, value)
-    assert instructor  # helps mypy
     user_request.lastInstructor = instructor
     db.session.add(user_request)
     db.session.flush()

api/src/pcapi/core/users/models.py
@@ -1122,7 +1122,7 @@ class UserAccountUpdateRequest(PcObject, Model):
     lastInstructorId: sa_orm.Mapped[int] = sa_orm.mapped_column(
         sa.BigInteger, sa.ForeignKey("user.id"), index=True, nullable=True
     )
-    lastInstructor: sa_orm.Mapped[User] = sa_orm.relationship(User, foreign_keys=[lastInstructorId])
+    lastInstructor: sa_orm.Mapped[User | None] = sa_orm.relationship(User, foreign_keys=[lastInstructorId])
     dateLastUserMessage: sa_orm.Mapped[datetime] = sa_orm.mapped_column(sa.DateTime, nullable=True)
     dateLastInstructorMessage: sa_orm.Mapped[datetime] = sa_orm.mapped_column(sa.DateTime, nullable=True)
     # Additional information to filter and/or show icons, badges...